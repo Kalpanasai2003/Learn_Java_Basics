cb15e2aedaa12177b27cece1003b8bfffd8b5962
Kalpanasai2003 <kalpanasai2003@gmail.com>
2025-08-01 17:03:55 +0530

    Update VariablesDemo.java with declared variables

---
+++ VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        //variable declaration  and initialization 
+        int age = 21;
+        String name = "Kalpana" ;
+        double height = 5.4 ;
+        char grade = 'A' ;
+        boolean isStudent = true ;
+           // print values 
+        System.out.println("Name :" + name);
+        System.out.println("Age :" + age);
+        System.out.println("Height :"+ height);
+        System.out.println("Grade :"+ grade);
+        System.out.println("Is Student :"+isStudent);
+    
+    }
+}
